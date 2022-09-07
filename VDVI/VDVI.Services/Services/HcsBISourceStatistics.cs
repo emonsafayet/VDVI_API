192d0dfe96df78e8c8e138b820385ca2248efce5
Safayet Hossain <emonsafayet@gmail.com>
2022-09-07 22:04:48 +0600

    .

---
+++ VDVI/VDVI.Services/Services/HcsBISourceStatistics.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using SOAPAppCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using VDVI.DB.IRepository;
+using VDVI.DB.Repository;
+using VDVI.Services.Interfaces;
+using static VDVI.DB.Models.ApmaModels.HcsBISourceStatisticsModel;
+
+namespace VDVI.Services.Services
+{
+    public class HcsBISourceStatistics : IHcsBISourceStatistics
+    {  
+        private readonly IConfiguration _config; 
+        private readonly IApmaTaskSchedulerService _apmaTaskSchedulerService;
+        public HcsBISourceStatistics(IConfiguration config,  IApmaTaskSchedulerService apmaTaskSchedulerService)
+        {
+            _config = config; 
+            _apmaTaskSchedulerService = apmaTaskSchedulerService;
+        }
+
+        public void GetHcsBISourceStatistics()
+        {
+            var list = new List<SourceStatistic>();
+        }
+    }
+}
