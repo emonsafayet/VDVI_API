36db0c2a964762b52797e4356077926a6d94e5de
Safayet Hossain <emonsafayet@gmail.com>
2022-09-14 15:37:58 +0600

    .

--- VDVI/VDVI.DB/Models/ApmaModels/Common/Audit.cs
+++ VDVI/VDVI.DB/Models/ApmaModels/Common/Audit.cs
@@ -5,7 +5,7 @@ namespace VDVI.Repository.Models.Common
     public class Audit
     {
         public long CreatedBy { get; set; }
-        public DateTime CreatedDateTime { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
         public long UpdatedBy { get; set; }
         public DateTime? UpdatedDateTime { get; set; }
     }
