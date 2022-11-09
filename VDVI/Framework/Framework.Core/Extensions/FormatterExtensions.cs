85f7394563a1e3a7e98066e102a5bcd1e8c5cac5
nahidbs23 <nahid.hasan@bs-23.net>
2022-11-09 14:22:03 +0600

    Nahid: Business refactoring of BoekingsdagenMutaties

---
+++ VDVI/Framework/Framework.Core/Extensions/FormatterExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Framework.Core.Extensions
+{
+    public static class FormatterExtensions
+    {
+        public static List<T> FormatList<T, TValue>(this List<T> list, Expression<Func<T, TValue>> memberLamda, TValue value)
+        {
+            list.ForEach(item => item.SetPropertyValue(memberLamda, value));
+            return list;
+        }
+
+        public static void SetPropertyValue<T, TValue>(this T target, Expression<Func<T, TValue>> memberLamda, TValue value)
+        {
+            var memberSelectorExpression = memberLamda.Body as MemberExpression;
+            if (memberSelectorExpression != null)
+            {
+                var property = memberSelectorExpression.Member as PropertyInfo;
+                if (property != null)
+                {
+                    property.SetValue(target, value, null);
+                }
+            }
+        }
+    }
+}
--- VDVI/VDVI.Services/Services/AfasServices/DmfBoekingsdagenMutatesService.cs
+++ VDVI/VDVI.Services/Services/AfasServices/DmfBoekingsdagenMutatesService.cs
@@ -10,19 +10,14 @@ using VDVI.Repository.Dtos.AfasDtos;
 using VDVI.Repository.Dtos.AfasDtos.AfasCommonDtos;
 using VDVI.Services.AfasInterfaces;
 using VDVI.Services.Services.BaseService;
+using DutchGrit.Afas;
+using Framework.Core.Extensions;
 
 namespace VDVI.Services.AfasServices
 {
     public class DmfBoekingsdagenMutatesService : AfasBaseService, IdmfBoekingsdagenMutatiesService
     {
         private readonly IdmFBoekingsdagenMutationService _dmFBoekingsdagenMutationService;
-
-        DMFBoekingsdagenMutatiesDto[]? AA;
-        DMFBoekingsdagenMutatiesDto[]? AC;
-        DMFBoekingsdagenMutatiesDto[]? AD;
-        DMFBoekingsdagenMutatiesDto[]? AE;
-        List<DMFBoekingsdagenMutatiesDto> Dto = new List<DMFBoekingsdagenMutatiesDto>();
-        private DateTime currentDate = DateTime.Now;
 
         public DmfBoekingsdagenMutatesService(IdmFBoekingsdagenMutationService dmFBoekingsdagenMutationService)
         {
@@ -36,13 +31,20 @@ namespace VDVI.Services.AfasServices
                 async () =>
                 {
 
-                    await LoadAsync(getConnector);
+                    var currentDate = DateTime.UtcNow.Date;
+
+                    List<DMFBoekingsdagenMutatiesDto> DtoList = new List<DMFBoekingsdagenMutatiesDto>();
+                    DtoList.AddRange(await GetBoekingsdagenMutatiesAsync(getConnector.clientAA, p => p.Datum_boeking <= currentDate, "AA"));
+                    DtoList.AddRange(await GetBoekingsdagenMutatiesAsync(getConnector.clientAC, p => p.Datum_boeking <= currentDate, "AC"));
+                    DtoList.AddRange(await GetBoekingsdagenMutatiesAsync(getConnector.clientAD, p => p.Datum_boeking <= currentDate, "AD"));
+                    DtoList.AddRange(await GetBoekingsdagenMutatiesAsync(getConnector.clientAE, p => p.Datum_boeking <= currentDate, "AE"));
+
                    
                     // DB operation
-                    if (Dto.Count > 0)
+                    if (DtoList.Count > 0)
                     {
-                        await _dmFBoekingsdagenMutationService.BulkInsertWithProcAsync(Dto, true);
-                        Dto.Clear();
+                        await _dmFBoekingsdagenMutationService.BulkInsertWithProcAsync(DtoList, true);
+                        DtoList.Clear();
                     }
 
 
@@ -56,51 +58,14 @@ namespace VDVI.Services.AfasServices
             );
 
         }
-        public async Task LoadAsync(AfasCrenditalsDto getConnector)
-        {
-            AA = await getConnector.clientAA.Query<DMFBoekingsdagenMutatiesDto>().Skip(-1).Take(-1).GetAsync();
-            if (AA.Length > 0) FormatSummaryObjectAA(AA.ToList(), Dto);
-            AC = await getConnector.clientAC.Query<DMFBoekingsdagenMutatiesDto>().Skip(-1).Take(-1).GetAsync();
-            if (AC.Length > 0) FormatSummaryObjectAC(AC.ToList(), Dto);
-            AD = await getConnector.clientAD.Query<DMFBoekingsdagenMutatiesDto>().Skip(-1).Take(-1).GetAsync();
-            if (AD.Length > 0) FormatSummaryObjectAD(AD.ToList(), Dto);
-            AE = await getConnector.clientAE.Query<DMFBoekingsdagenMutatiesDto>().Skip(-1).Take(-1).GetAsync();
-            if (AE.Length > 0) FormatSummaryObjectAE(AE.ToList(), Dto);
 
-            
-        }
+        private async Task<List<DMFBoekingsdagenMutatiesDto>> GetBoekingsdagenMutatiesAsync(AfasClient Client, Func<DMFBoekingsdagenMutatiesDto, bool> predicate, string environmentCode)
+        {
+            var result = (await Client.Query<DMFBoekingsdagenMutatiesDto>().Skip(-1).Take(-1).GetAsync()).ToList().Where(predicate).ToList();
+            if (result.Count > 0)
+                return result.FormatList(x => x.Omgeving_code, environmentCode);
 
-        private void FormatSummaryObjectAA(List<DMFBoekingsdagenMutatiesDto> _AA, List<DMFBoekingsdagenMutatiesDto> _Dto)
-        {
-            if (_AA.Count > 0)
-            { 
-                _AA.ForEach(a => a.Omgeving_code = "AA");
-                _Dto.AddRange(_AA);
-            }
-        }
-        private void FormatSummaryObjectAC(List<DMFBoekingsdagenMutatiesDto> _AC, List<DMFBoekingsdagenMutatiesDto> _Dto)
-        {
-            if (_AC.Count > 0)
-            {
-                _AC.ForEach(a => a.Omgeving_code = "AC");
-                _Dto.AddRange(_AC);
-            }
-        }
-        private void FormatSummaryObjectAD(List<DMFBoekingsdagenMutatiesDto> _AD, List<DMFBoekingsdagenMutatiesDto> _Dto)
-        {
-            if (_AD.Count > 0)
-            {
-                _AD.ForEach(a => a.Omgeving_code = "AD");
-                _Dto.AddRange(_AD);
-            }
-        }
-        private void FormatSummaryObjectAE(List<DMFBoekingsdagenMutatiesDto> _AE, List<DMFBoekingsdagenMutatiesDto> _Dto)
-        {
-            if (_AE.Count > 0)
-            {
-                _AE.ForEach(a => a.Omgeving_code = "AE");
-                _Dto.AddRange(_AE);
-            }
+            return new List<DMFBoekingsdagenMutatiesDto>();
         }
     }
 }
